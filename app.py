947a03a6deaafcdd8c72337cbcae57f7cb70e2f3
arnabmshop <137401894+arnabmshop@users.noreply.github.com>
2023-06-22 18:39:51 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,3 @@
-pip install flask azure-storage-blob
 from flask import Flask, render_template, request, send_file
 from azure.storage.blob import BlobServiceClient, BlobClient
 
@@ -28,23 +27,23 @@ def upload():
         return 'No file selected'
 
     # Upload the file to Azure Blob Storage
-    blob_client = blob_service_client.get_blob_client(container=CONTAINER_NAME, blob=file.filename)
-    blob_client.upload_blob(file)
+    # blob_client = blob_service_client.get_blob_client(container=CONTAINER_NAME, blob=file.filename)
+    # blob_client.upload_blob(file)
 
     return 'File uploaded successfully'
 
 @app.route('/download/<filename>')
 def download(filename):
     # Download the file from Azure Blob Storage
-    blob_client = blob_service_client.get_blob_client(container=CONTAINER_NAME, blob=filename)
-    file_stream = blob_client.download_blob().readall()
-
-    return send_file(
-        file_stream,
-        mimetype=blob_client.blob_properties.content_type,
-        as_attachment=True,
-        attachment_filename=filename
-    )
+    # blob_client = blob_service_client.get_blob_client(container=CONTAINER_NAME, blob=filename)
+    # file_stream = blob_client.download_blob().readall()
+    return send_file()
+    # return send_file(
+    #     file_stream,
+    #     mimetype=blob_client.blob_properties.content_type,
+    #     as_attachment=True,
+    #     attachment_filename=filename
+    # )
 
 if __name__ == '__main__':
     app.run(debug=True)
